374ff51d0379aca3072c9a86951ed96ab5a28283
omarRaslan66 <omarraslan660@gmail.com>
2026-01-08 21:55:48 +0200

    Update nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -3,7 +3,6 @@
 
 <head>
     <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- google font -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -12,7 +11,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Almendra:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet" />
 
-
     <!-- site icon -->
     <link rel="icon" type="image/png" href="../assets/icons/logoSCCI.png" />
 
@@ -20,7 +18,7 @@
     <link rel="stylesheet" href="../assets/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/root.css">
     <!-- css page link -->
-    <link rel="stylesheet" href="../assets/css/navbar.css" />
+    <link rel="stylesheet" href="../assets/css/nav.css" />
 
     <title></title>
 </head>
@@ -36,19 +34,19 @@
         </a>
 
         <nav class="navLinks navRespnsive">
-            <a href="home.html" id="homeNavLine" href="#">home</a>
-            <a href="about.html" id="aboutNavLine">about us</a>
-            <a href="gallery.html" id="galleryNavLine">gallery</a>
-            <a href="workshops.html" id="workshopsNavLine">workshops</a>
-            <a href="crew.html" id="crewNavLine">crew</a>
+            <a href="../home.php" id="homeNavLine">home</a>
+            <a href="../about.php" id="aboutNavLine">about us</a>
+            <a href="../gallary.php" id="galleryNavLine">gallery</a>
+            <a href="../workshops.php" id="workshopsNavLine">workshops</a>
+            <a href="../crew.php" id="crewNavLine">crew</a>
 
         </nav>
 
         <nav class="navAccount navRespnsive">
-            <a href="login.html" id="loginNav" href="#">Log In</a>
+            <a href="../auth/login.php" id="loginNav">Log In</a>
             <!-- profile -->
-            <a href="profile.html" id="profileNav">
-                <img src="../assets/img/profilePhoto.png" alt="">
+            <a href="../crew.php" id="profileNav">
+                <img loading="lazy" src="../assets/img/profilePhoto.png" alt="">
             </a>
         </nav>
 
@@ -63,24 +61,24 @@
 
             <p class="closeNav">X</p>
             <hr>
-            <a href="home.html"><i class="fa-solid fa-house"></i> home</a>
-            <a href="about us.html"><i class="fa-solid fa-book-open"></i> about us</a>
-            <a href="gallery.html"><i class="fa-solid fa-images"></i> gallery</a>
-            <a href="workshops.html"><i class="fa-solid fa-graduation-cap"></i> workshops</a>
-            <a href="crew.html"><i class="fa-solid fa-users"></i> crew</a>
+            <a href="../home.php"><i class="fa-solid fa-house"></i> home</a>
+            <a href="../about.php"><i class="fa-solid fa-book-open"></i> about us</a>
+            <a href="../gallary.php"><i class="fa-solid fa-images"></i> gallery</a>
+            <a href="../workshops.php"><i class="fa-solid fa-graduation-cap"></i> workshops</a>
+            <a href="../crew.php"><i class="fa-solid fa-users"></i> crew</a>
             <!-- login -->
-            <a href="login.html"><i class="fa-solid fa-user"></i> LogIn</a>
+            <a href="../auth/login.php" src="../"><i class="fa-solid fa-user"></i> LogIn</a>
             <!-- profile -->
             <hr>
-            <a href="profile.html" id="profileNav">
-                <img src="../assets/img/profilePhoto.png" alt="">
+            <a href="../crew.php" id="profileNav">
+                <img loading="lazy" src="../assets/img/profilePhoto.png" alt="">
             </a>
         </div>
     </aside>
 
     <!-- navbar js -->
      
-    <script src="../assets/js/navbar.js"></script>
+    <script src="../assets/js/nav.js"></script>
 
     <script src="../assets/js/all.min.js"></script>
 
