66898ef2190bd2df2ff861837d9388a67e47ccc9
AwadCoding <112626336+AwadCoding@users.noreply.github.com>
2026-01-12 21:49:31 +0200

    edit aa

--- auth/registerParticipant.php
+++ auth/registerParticipant.php
@@ -67,7 +67,8 @@ $run_w = mysqli_query($connect, $select_w);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>SCCI - Register</title>
+    <link rel="icon" href="assets/icons/logoSCCI.png" type="image/png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Irish+Grover&display=swap" rel="stylesheet">
